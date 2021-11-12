535966e1041321ce51bd5769f17502981514b28a
sarvesh <salam.sarvesh@gmail.com>
2021-11-12 12:39:26 +0530

    12 nov

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import os 
+import os
 
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
-
 folder = '1xV_tRXPbjRurN9Tn1a_14eiUTN7zhRAG'
+file1 = drive.CreateFile({'title': '11AA1113',
+                          'mimeType': "application/vnd.google-apps.folder",
+                          'parents': [{"kind": "drive#fileLink", "id": folder}]})
+file1.Upload()
+folder_id = file1['id']
+file2 = drive.CreateFile({'title': 'filename.jpg',
+                          'mimeType': 'image/jpeg',
+                          'parents': [{"kind": "drive#fileLink", "id": folder_id}]})
+file2.SetContentFile('721078.jpg')
+file2.Upload()
+permission=file2.InsertPermission({
+                            'type': 'anyone',
+                            'value': 'anyone',
+                            'role': 'reader'})
 
-# file1 = drive.CreateFile({'parents' : [{'id' : folder}], 'title' : 'hello2.txt'})
-# file1.SetContentString('Hello world!, this is my second file')
-# file1.Upload()
-
-directory = "C:/Users/sarve/OneDrive/Pictures/Camera Roll"
-for f in os.listdir(directory):
-    filepath = os.path.join(directory,f)
-    # print(filepath)
-    gfile = drive.CreateFile({'parents' : [{'id' : folder}], 'title' : f})
-    gfile.SetContentFile(filepath)
-    gfile.Upload()
+link=file2['alternateLink']
+link=link.split('?')[0]
+link=link.split('/')[-2]
+link='https://drive.google.com/file/d/'+link+'/view?usp=sharing'
+print(link)
